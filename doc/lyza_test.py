2aa3e6fff58e27c7c0adeb6190fc49e75e8af4de
jaseg <git@jaseg.net>
2018-07-17 15:17:23 +0200

    Modify lyza-test script for the olsndots

--- doc/lyza_test.py
+++ doc/lyza_test.py
@@ -6,9 +6,10 @@ import sqlite3
 from olsndot import Olsndot, Driver
 from datetime import datetime
 
-from pyBusPirateLite import Buspirate
+uut1 = Olsndot(0x23420001)
+uut2 = Olsndot(0x23420002)
+d = Driver('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0', devices=[uut1, uut2])
 
-uut = Olsndot(0xDEBE10BB)
-d = Driver('/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0', devices=[uut])
-
+print('uut 1:', uut1.fetch_status())
+print('uut 2:', uut2.fetch_status())
 
